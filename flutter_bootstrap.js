449a0809b58ea9003d9eff25a0a354e8723d4b22
Edoardo Fabrizio De Iovanna <edoardofabriziodeiovanna@hotmail.com>
2024-06-08 22:49:36 -0300

    first commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2933495027"
+    serviceWorkerVersion: "1929293848"
   }
 });
